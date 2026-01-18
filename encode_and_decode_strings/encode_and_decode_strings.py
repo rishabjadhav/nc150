b16be4542261edd0748ea33988b38cadd6d00207
Rishab Jadhav <rishabjadhav@gmail.com>
2026-01-18 15:15:32 -0600

    redo encode and decode strings

--- encode_and_decode_strings/encode_and_decode_strings.py
+++ encode_and_decode_strings/encode_and_decode_strings.py
@@ -1,35 +1,34 @@
 class Solution:
-    def encode(self, strs: List[str]) -> str:
-        # to hold the resulting string
+    def encode(self, strs: List[str]) -> str: # encode a list of strings into one string
         res = ""
-
-        for s in strs:
-            # for each element in strs, append the number of characters followed by a # delimiter
-            res += str(len(s)) + "#" + s
         
+        for s in strs:
+            # a string will be encoded with it's length, a # delimiter, and the string itself
+            # format: n#string, where n is len(string)
+            res += str(len(s)) + "#" + s
+
         return res
 
-    def decode(self, s: str) -> List[str]:
-        res = []
+    def decode(self, s: str) -> List[str]: # decode that string back into a list of strings
+        strs = []
 
-        # i will hold our place in the string as we parse through it
+        # right pointer, between previous string and current string length
         i = 0
 
+        # right pointer should not exceed string length
         while i < len(s):
-            # j is a pointer to parse length of word
+
+            # left pointer, between string length and string
             j = i
-            
-            # move j up until it gets to the delimiter
+
+            # find end of string length
             while s[j] != "#":
                 j += 1
-            
-            # the length of the word is given by the integer present until #
-            length = int(s[i:j])
 
-            # the word can be isolated as a substring, starting at j + 1 (after #) and has length "length"
-            res.append(s[j + 1: j + 1 + length])
-            
-            # shift up pointer i to the start of the next word
-            i = j + 1 + length
-        
-        return res
+            strlen = int(s[i:j])
+
+            strs.append(s[j+1: j+1+strlen])
+
+            i = j + 1 + strlen
+
+        return strs
